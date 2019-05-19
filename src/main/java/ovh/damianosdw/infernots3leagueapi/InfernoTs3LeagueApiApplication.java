ca495e65c9c2ceec054146382e71ca57e447d7ae
DamianosDW <damianosdw@gmail.com>
2019-05-19 19:04:21 +0200

    Add main files to repo

---
+++ src/main/java/ovh/damianosdw/infernots3leagueapi/InfernoTs3LeagueApiApplication.java
@@ -0,0 +1,13 @@
+package ovh.damianosdw.infernots3leagueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfernoTs3LeagueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfernoTs3LeagueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ovh/damianosdw/infernots3leagueapi/InfernoTs3LeagueApiApplicationTests.java
@@ -0,0 +1,17 @@
+package ovh.damianosdw.infernots3leagueapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfernoTs3LeagueApiApplicationTests
+{
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
