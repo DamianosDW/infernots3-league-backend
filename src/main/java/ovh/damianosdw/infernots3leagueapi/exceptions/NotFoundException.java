6b8e5348e197b50a8065a356c8a0f2759f28fa8d
DamianosDW <damianosdw@gmail.com>
2019-05-20 14:35:28 +0200

    Create custom exceptions

---
+++ src/main/java/ovh/damianosdw/infernots3leagueapi/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package ovh.damianosdw.infernots3leagueapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found in database!")
+public class NotFoundException extends NullPointerException
+{
+
+}
---
+++ src/main/java/ovh/damianosdw/infernots3leagueapi/exceptions/UserFoundException.java
@@ -0,0 +1,10 @@
+package ovh.damianosdw.infernots3leagueapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "This user is in database!")
+public class UserFoundException extends IllegalArgumentException
+{
+
+}
