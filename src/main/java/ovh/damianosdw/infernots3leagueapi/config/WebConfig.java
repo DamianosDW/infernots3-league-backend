8950b25aefa27e55c4db5bd7e8eaddcdae9f7b24
DamianosDW <damianosdw@gmail.com>
2019-05-27 13:07:20 +0200

    Create web mvc config and remove @CrossOrigin annotation

---
+++ src/main/java/ovh/damianosdw/infernots3leagueapi/config/WebConfig.java
@@ -0,0 +1,22 @@
+/*
+ * Created by DamianosDW
+ * https://damianosdw.ovh
+ */
+
+package ovh.damianosdw.infernots3leagueapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**");
+    }
+}
--- src/main/java/ovh/damianosdw/infernots3leagueapi/endpoints/UsersController.java
+++ src/main/java/ovh/damianosdw/infernots3leagueapi/endpoints/UsersController.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/users")
 @AllArgsConstructor
 public class UsersController
