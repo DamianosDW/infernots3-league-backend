15ab7390c4a33a05e19ef147b4069c3a04be7f76
DamianosDW <damianosdw@gmail.com>
2019-06-03 18:31:52 +0200

    Create REST controller - MatchesController

---
+++ src/main/java/ovh/damianosdw/infernots3leagueapi/endpoints/MatchesController.java
@@ -0,0 +1,65 @@
+/*
+ * Created by DamianosDW
+ * https://damianosdw.ovh
+ */
+
+package ovh.damianosdw.infernots3leagueapi.endpoints;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ovh.damianosdw.infernots3leagueapi.db.dbmodels.Match;
+import ovh.damianosdw.infernots3leagueapi.db.repositories.MatchesRepository;
+import ovh.damianosdw.infernots3leagueapi.misc.UserInfo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/matches")
+@AllArgsConstructor
+public class MatchesController
+{
+    private final MatchesRepository matchesRepository;
+
+    @GetMapping("")
+    public List<Match> getAllCurrentMatches()
+    {
+        return matchesRepository.findAll().stream()
+                .filter(match -> match.getMatchStartDate().isAfter(LocalDateTime.now().minusWeeks(1)))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("user/{userId}/opponent")
+    public UserInfo getAvailableOpponent(@PathVariable("userId") int userId)
+    {
+        return matchesRepository.getOpponent(userId);
+    }
+
+    @GetMapping("{userId}/nextMatch")
+    public Match getUserNextMatchByUserId(@PathVariable("userId") int userId)
+    {
+        List<Match> currentMatches = getAllCurrentMatches();
+
+        return currentMatches.stream()
+                .filter(match -> match.getFirstPlayer().getUserId() == userId || match.getSecondPlayer().getUserId() == userId)
+                .collect(Collectors.toList()).get(0);
+    }
+
+    @PostMapping("create")
+    public void createMatch(@RequestBody Match match)
+    {
+        matchesRepository.save(match);
+    }
+
+    @PutMapping("{matchId}")
+    public boolean endMatch(@PathVariable("matchId") int matchId)
+    {
+        try {
+            matchesRepository.endMatch(matchId);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+}
