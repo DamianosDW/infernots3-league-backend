29941061cd745a52161463cd80e422486466eb86
DamianosDW <damianosdw@gmail.com>
2019-06-03 18:25:55 +0200

    Create getAllLolNicknames() - custom UsersRepository

--- src/main/java/ovh/damianosdw/infernots3leagueapi/db/custom/UsersRepositoryCustom.java
+++ src/main/java/ovh/damianosdw/infernots3leagueapi/db/custom/UsersRepositoryCustom.java
@@ -7,7 +7,10 @@ package ovh.damianosdw.infernots3leagueapi.db.custom;
 
 import ovh.damianosdw.infernots3leagueapi.db.dbmodels.User;
 
+import java.util.List;
+
 public interface UsersRepositoryCustom
 {
     void updateUsers(User user);
+    List<String> getAllLolNicknames();
 }
--- src/main/java/ovh/damianosdw/infernots3leagueapi/db/custom/UsersRepositoryCustomImpl.java
+++ src/main/java/ovh/damianosdw/infernots3leagueapi/db/custom/UsersRepositoryCustomImpl.java
@@ -6,6 +6,8 @@ import ovh.damianosdw.infernots3leagueapi.db.dbmodels.User;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.LinkedList;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -26,5 +28,18 @@ public class UsersRepositoryCustomImpl implements UsersRepositoryCustom
                 .setParameter(6, user.getUserId())
                 .executeUpdate();
 
+    }
+
+    @Override
+    public List<String> getAllLolNicknames()
+    {
+        List data = entityManager.createNativeQuery("SELECT LOL_NICKNAME FROM USERS").getResultList();
+        List<String> lolNicknames = new LinkedList<>();
+
+        for(Object lolNickname : data)
+        {
+            lolNicknames.add(String.valueOf(lolNickname));
+        }
+        return lolNicknames;
     }
 }
